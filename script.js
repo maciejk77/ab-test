1f6a520b0024059dfc17a4b604c64126c82c6729
Maciej Kurek <maciejk77@gmail.com>
2016-11-15 00:48:33 +0000

    Ver4, Fix deleteScenario, session logic implemented

--- script.js
+++ script.js
@@ -9,19 +9,22 @@ function runTest() {
   var test = pattern.test(toDoElements[randomInt]);
   
   // Shows which test has been rendered to the screen
-  console.log('changed element at index ' + randomInt + ' to ' + toDoElements[randomInt]);
-  // Control array of scenarios 
-  console.log(testsNum);
-
-  getScenario();
+  console.log('changed element at index ------> ' + randomInt + ' to ' + toDoElements[randomInt]);
 
   if(sessionStorage.length < 1) {
     sessionStorage.setItem('baseArray', baseElements);    
+    sessionStorage.setItem('testsNumElements', testsNum);
     sessionStorage.setItem('toDoArray', toDoElements);
   }
+
+  // Control array of scenarios 
+  console.log('toDoArray is now ====>' + sessionStorage.getItem('toDoArray'));
+
+  getScenario();
   
   function getScenario() {
     test ? setImage() : setText();
+    deleteScenario();
   }
 
   function getRandomInt() {
@@ -46,6 +49,25 @@ function runTest() {
   function removeTestNum(number) {
     for(var i=0; i<testsNum.length; i++) {
       if(testsNum[i] === number) { testsNum.splice(i,1) };
+    }
+  }
+
+  function deleteScenario() {
+    var scenarios = sessionStorage.getItem('testsNumElements').split(',');
+    scenarios.splice(randomInt, 1);
+    var result = scenarios.join();
+    sessionStorage.setItem('testsNumElements', result);
+
+    // var elements = sessionStorage.getItem('toDoArray').split(',');
+    // elements.splice(randomInt, 1);
+    // var res = elements.join();
+    // sessionStorage.setItem('toDoArray', res);
+
+    console.log(sessionStorage.getItem('testsNumElements') + ' <--- testsNum array');
+    // console.log(sessionStorage.getItem('toDoArray'));
+
+    if(scenarios.length < 1) {
+      sessionStorage.setItem('testsNumElements', testsNum);
     }
   }
 
